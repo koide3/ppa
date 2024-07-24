106076e1074bdd1d2dce84c8785bd63fabf59b53
k.koide <k.koide@aist.go.jp>
2024-07-24 18:48:48 +0900

    add setup_ppa.sh

---
+++ setup_ppa.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# exit if not root
+if [ "$EUID" -ne 0 ]; then
+  echo "Must be run with sudo"
+  exit 1
+fi
+
+apt-get update && apt-get install -y curl gpg
+
+. /etc/os-release
+release="${UBUNTU_CODENAME}"
+echo release=$release
+
+if [ $release == "noble" ]; then
+  ubuntu_label="ubuntu2404"
+elif [ $release == "jammy" ]; then
+  ubuntu_label="ubuntu2204"
+elif [ $release == "focal" ]; then
+  ubuntu_label="ubuntu2004"
+else
+  echo "Unsupported release: $release"
+  exit 1
+fi
+
+echo "ubuntu_label=$ubuntu_label"
+echo curl -s --compressed "https://koide3.github.io/ppa/$ubuntu_label/KEY.gpg" | gpg --dearmor | tee /etc/apt/trusted.gpg.d/koide3_ppa.gpg >/dev/null
+echo echo "deb [signed-by=/etc/apt/trusted.gpg.d/koide3_ppa.gpg] https://koide3.github.io/ppa/$ubuntu_label ./" | tee /etc/apt/sources.list.d/koide3_ppa.list
+
+curl -s --compressed "https://koide3.github.io/ppa/$ubuntu_label/KEY.gpg" | gpg --dearmor | tee /etc/apt/trusted.gpg.d/koide3_ppa.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/koide3_ppa.gpg] https://koide3.github.io/ppa/$ubuntu_label ./" | tee /etc/apt/sources.list.d/koide3_ppa.list
